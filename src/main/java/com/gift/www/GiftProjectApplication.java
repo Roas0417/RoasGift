4c3051d432e9bf8b17cf6b80e304154b59dfc8c7
velykitty <jiyunee13@gmail.com>
2020-08-18 11:42:46 +0900

    first commit

---
+++ src/main/java/com/gift/www/GiftProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gift.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gift/www/GiftProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gift.www;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiftProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
