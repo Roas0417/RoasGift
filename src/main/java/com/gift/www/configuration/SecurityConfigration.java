07ffe26170a16d9d2eb25d32053946a8271b9115
sunjuho <sun_juho@naver.com>
2020-08-28 10:04:39 +0900

    카카오 로그인 시도중.

---
+++ src/main/java/com/gift/www/configuration/SecurityConfigration.java
@@ -0,0 +1,31 @@
+package com.gift.www.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfigration extends WebSecurityConfigurerAdapter {
+	
+//    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable().headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/assets/**", "/h2-console/**").permitAll()
+//                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                ;
+    }
+
+}
